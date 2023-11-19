5bf3a2f750994074dedc8e46c70865651c76d150
marouaneMOQRANE <127626651+marouaneMOQRANE@users.noreply.github.com>
2023-11-19 11:49:40 +0100

    Update settings.py

--- projet/settings.py
+++ projet/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5r$_8pn04xsd^3gpr-(rp-r6o-4g&&6(t&n^9k1u2_jnxj7^^n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [54.83.130.26']
+ALLOWED_HOSTS = ['54.83.130.26']
 
 
 # Application definition
